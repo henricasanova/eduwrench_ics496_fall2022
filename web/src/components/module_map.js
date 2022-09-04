566dfd3077a8570e5f341bf2e9bad00faadab204
Yeji Han <hany7@hawaii.edu>
2022-09-04 13:27:51 -1000

    added nodes

--- web/src/components/module_map.js
+++ web/src/components/module_map.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useRef } from 'react';
+// import { useRef } from 'react';
 import CytoscapeComponent from 'react-cytoscapejs';
 
 const ModuleMap = () => {
@@ -8,23 +8,108 @@ const ModuleMap = () => {
       {
         data: {
           id: '1',
-          label: 'node1',
+          label: 'A.1',
           href: '/pedagogic_modules/single_core_computing/'
         },
         position: {
-          x: 100,
-          y: 100
+          x: 543.5,
+          y: 409
         },
       },
       {
         data: {
           id: '2',
-          label: 'node2',
+          label: 'A.3.1',
           href: '/pedagogic_modules/single_core_computing/'
         },
         position: {
-          x: 200,
-          y: 100
+          x: 261.75,
+          y: 838
+        },
+      },
+      {
+        data: {
+          id: '3',
+          label: 'A.2',
+          href: '/pedagogic_modules/single_core_computing/'
+        },
+        position: {
+          x: 543.5,
+          y: 838
+        },
+      },
+      {
+        data: {
+          id: '4',
+          label: 'A.3.2',
+          href: '/pedagogic_modules/single_core_computing/'
+        },
+        position: {
+          x: 261.75,
+          y: 1267
+        },
+      },{
+        data: {
+          id: '5',
+          label: 'A.3.3',
+          href: '/pedagogic_modules/single_core_computing/'
+        },
+        position: {
+          x: 543.5,
+          y: 1267
+        },
+      },{
+        data: {
+          id: '6',
+          label: 'A.3.4',
+          href: '/pedagogic_modules/single_core_computing/'
+        },
+        position: {
+          x: 825.25,
+          y: 1267
+        },
+      },{
+        data: {
+          id: '7',
+          label: 'C.1',
+          href: '/pedagogic_modules/single_core_computing/'
+        },
+        position: {
+          x: 1107,
+          y: 1267
+        },
+      },
+      {
+        data: {
+          id: '8',
+          label: 'B.1',
+          href: '/pedagogic_modules/single_core_computing/'
+        },
+        position: {
+          x: 261.75,
+          y: 1696
+        },
+      },
+      {
+        data: {
+          id: '9',
+          label: 'C.2',
+          href: '/pedagogic_modules/single_core_computing/'
+        },
+        position: {
+          x: 543.5,
+          y: 1696
+        },
+      },
+      {
+        data: {
+          id: '10',
+          label: 'D.1',
+          href: '/pedagogic_modules/single_core_computing/'
+        },
+        position: {
+          x: 1107,
+          y: 1696
         },
       },
     ],
@@ -39,7 +124,7 @@ const ModuleMap = () => {
     ],
   };
 
-  const cyRef = useRef();
+  // const cyRef = useRef();
 
   return (
       <>
@@ -58,7 +143,7 @@ const ModuleMap = () => {
 				      });
 			      }}
 
-            style={{ width: '400px', height: '400px' }}
+            style={{ width: '1172px', height: '1716px' }}
 
         />
       </>
